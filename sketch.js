801b3e3ff95111aa830bb2af660987e62688c22a
yusuf17-10 <64640684+yusuf17-10@users.noreply.github.com>
2020-07-30 14:16:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var drawing=[];
+var beginShape;
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+  
+  if(mouseIsPressed){
+      var point ={
+        x: mouseX,
+        y: mouseY
+  }
+      drawing.push(point);
+}
+      beginShape();
+      stroke(255);
+      strokeWeight(4);
+      noFill();
+      for (var i = 0; i<drawing.length;i++){
+        vertex(drawing[i].x,drawing[i].y)
+      }
+      endShape();
+
+}
